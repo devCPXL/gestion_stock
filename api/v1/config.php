ad9301d6feed74b2ab41c2227b7a052e3e8a5249
ekiemma <ekiemma@gmail.com>
2016-06-03 11:25:17 +0200

    change Host name

--- api/v1/config.php
+++ api/v1/config.php
@@ -4,7 +4,7 @@
  */
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
-define('DB_HOST', 'cpasxldb');
+define('DB_HOST', 'cpasxldbtest');
 define('DB_NAME', 'cpas_test');
 define('DB_NAME_TEST', 'test');
 
