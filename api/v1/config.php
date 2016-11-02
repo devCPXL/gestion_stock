fa0129c1eb33e162043e6d60dff4f533c8dcdfff
ekiemma <ekiemma@gmail.com>
2016-11-02 12:12:41 +0100

    update config

--- api/v1/config.php
+++ api/v1/config.php
@@ -2,10 +2,10 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_HOST', 'cpasxldbtest');//'10.102.96.47'); // 'cpasxldbtest'
-define('DB_NAME', 'cpas_test');
-define('DB_NAME_TEST', 'test');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_NAME_TEST', '');
 
 ?>
